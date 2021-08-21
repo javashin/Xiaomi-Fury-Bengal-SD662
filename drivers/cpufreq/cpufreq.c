c9d3955e5b457b9368f2fda53b003e40353cb053
Sultan Alsawaf <sultanxda@gmail.com>
2021-08-21 17:03:07 -0400

    cpufreq: Kill userspace CPU boosting entirely
    Kernel-based CPU boosting is used now, so stop userspace from messing with
    it by turning scaling_min_freq into a no-op. Note that this is done instead
    of making scaling_min_freq read-only so that userspace doesn't spit out
    error messages when it can't do its boosting.
    
    Signed-off-by: Sultan Alsawaf <sultanxda@gmail.com>
    Signed-off-by: Adam W. Willis <return.of.octobot@gmail.com>
    Signed-off-by: billaids <jimmy.nelle@hsw-stud.de>
    Change-Id: I8b83f15f805ff97646e8626fe20f7bb0be0de98c
    Signed-off-by: Carlos Jimenez (JavaShin-X) <javashin1986@gmail.com>

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -723,6 +723,9 @@ static ssize_t store_##file_name					\
 {									\
 	int ret, temp;							\
 	struct cpufreq_policy new_policy;				\
+                                                                        \
+	if (&policy->object == &policy->min)				\
+		return count;						\
 									\
 	if (&policy->object == &policy->min)				\
 		return count;						\
