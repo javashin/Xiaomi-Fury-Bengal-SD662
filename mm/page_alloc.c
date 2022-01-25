13ef1f26ec4a33bd3c64ab6425938480ad6f6024
Tyler Nijmeh <tylernij@gmail.com>
2022-01-25 14:45:11 -0400

    mm: Increase watermark scale factor
    By default, kswapd awakens once only 0.1% of memory is available. For
    heavy multitasking and unpredictable operating systems such as Android,
    it is difficult to predict if userspace will request a large chunk of
    memory in a short period of time.
    
    If the kernel runs out of available space, and kswapd is not triggered
    soon enough, a direct reclaimation will be required, which is taxing and
    expensive on the system.
    
    To avoid direct reclaimation, we can awaken kswapd sooner. Increase the
    watermark scale factor to 1% instead of 0.1% of remailing available
    memory. For an 8 GB system, this would be 80 MB of remaining memory.
    
    Referenced sources: https://source.android.com/devices/tech/perf/low-ram
    
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>
    Signed-off-by: Carlos Jimenez (JavaShin-X) <javashin1986@gmail.com>

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -338,7 +338,7 @@ int watermark_boost_factor __read_mostly;
 #else
 int watermark_boost_factor __read_mostly = 15000;
 #endif
-int watermark_scale_factor = 10;
+int watermark_scale_factor = 100;
 
 /*
  * Extra memory for the system to try freeing. Used to temporarily
