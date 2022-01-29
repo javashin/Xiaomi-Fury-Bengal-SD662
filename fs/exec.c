740bff6809e438f262bd645148b80485315ca140
John Galt <johngaltfirstrun@gmail.com>
2022-01-29 19:51:32 -0400

    fs/exec: properly make fingerprint prefix a prefix
    * Makes the prefix more general
    
    Signed-off-by: John Galt <johngaltfirstrun@gmail.com>
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>
    Signed-off-by: Carlos Jimenez (JavaShin-X) <javashin1986@gmail.com>

--- fs/exec.c
+++ fs/exec.c
@@ -80,7 +80,7 @@ static DEFINE_RWLOCK(binfmt_lock);
 #define HWCOMPOSER_BIN_PREFIX "/vendor/bin/hw/android.hardware.graphics.composer"
 #define ZYGOTE32_BIN "/system/bin/app_process32"
 #define ZYGOTE64_BIN "/system/bin/app_process64"
-#define FP_BIN_PREFIX "/vendor/bin/hw/android.hardware.biometrics.fingerprint@2.1-service"
+#define FP_BIN_PREFIX "/vendor/bin/hw/android.hardware.biometrics.fingerprint"
 static struct signal_struct *zygote32_sig;
 static struct signal_struct *zygote64_sig;
 
